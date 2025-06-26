0b483cd6281eb330cae8f9932fa46658886ff1b3
Anisha <anishachowdhury34gmail.com>
2025-06-27 02:39:57 +0530

    Added

---
+++ data/index.ts
@@ -0,0 +1,183 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+export const testimonials = [
+  {
+    quote:
+      "Working with Anisha on our recent project was an incredible experience. She is not only technically strong but also highly creative when it comes to problem-solving. Her attention to detail in both the backend logic and frontend design really elevated the quality of our project. Whether it was debugging complex code or designing user-friendly interfaces, Anisha was always proactive and solutions-driven. Her communication skills and team coordination made the entire collaboration smooth and enjoyable. I would love to work with her again on future projects!",
+    name: "Rita Singh",
+    title: "Student of IIT Delhi",
+  },
+  {
+    quote:
+      "Collaborating with Anisha on our project was a great learning experience. She has this rare ability to balance both development and design seamlessly. During the project, she took ownership of the challenging tasks and never hesitated to help others when we got stuck. Her problem-solving approach, timely communication, and eye for clean UI/UX design really made a difference in delivering the project successfully. Anisha’s dedication and teamwork were truly inspiring",
+    name: "Project Collaborator",
+    title: "Akash Nath",
+  },
+  {
+    quote:
+      "Working with Anisha on our project was a fantastic experience. She brings a perfect mix of technical expertise and creativity to the table. Throughout the project, she was extremely proactive in solving complex issues and contributed heavily to both frontend and backend development. Her ability to quickly adapt to new tools and frameworks helped us overcome tight deadlines. Anisha’s leadership in coordinating tasks and keeping the entire team motivated made a huge impact on the success of our project.",
+    name: "Priya Verma",
+    title: "Full Stack Developer & Project Collaborator"
+
+
+  },
+ 
+];
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+export const workExperience = [
+  {
+    id: 1,
+    title: "Engineering trainee ",
+    desc: "Assisting in the maintenance and troubleshooting of servers, networks, and computer systems across the plant.Supporting cybersecurity measures for plant networks and critical IT assets.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+ 
+];
+export const gridItems = [
+  {
+    id: 1,
+    title: "Strong collaboration and open communication are at the heart of my workflow, ensuring every project reflects the client’s goals.",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a project to validate and verify testimonials by clients and customers",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+export const projects = [
+  {
+    id: 1,
+    title: "MockPrep-AI Interview Platform",
+    des: "MockPrep is an intelligent, voice-enabled mock interview platform built for job seekers.",
+    img: "/Screenshot 2025-06-24 010908.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "https://mock-prep-interview-prep.vercel.app/",
+  },
+  {
+    id: 2,
+    title: "TrackNest-Finance manager",
+    des: "TrackNest is a simple and intuitive budget management web app that helps users track their income, expenses, and savings goals in real-time.",
+    img: "/Screenshot 2025-06-26 034907.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "https://budget-tracking-app-pxfr.vercel.app/",
+  },
+  {
+    id: 3,
+    title: "Brain Tumor Network",
+    des: "This is an AI-powered web platform that allows users to upload MRI images and instantly get predictions about the presence and type of brain tumor. ",
+    img: "Screenshot 2025-06-26 035451.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "https://brain-tumor-network-gxuc.vercel.app/",
+  },
+  {
+    id: 4,
+    title: "Realtime Collaborating Whiteboard",
+    des: "This is a web-based drawing platform that allows multiple users to draw, sketch, and brainstorm together in real time.",
+    img: "/Screenshot 2025-06-26 140041.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "/ui.apple.com",
+  },
+];
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
+
---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://7609d468e047cc27b47d008f30aef72c@o4509391087206400.ingest.de.sentry.io/4509391089041488",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
