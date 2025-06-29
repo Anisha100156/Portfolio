6d05417e8ee78cb540f0b2d5d0a1f11ac1b6338e
Anisha <anishachowdhury34gmail.com>
2025-06-30 04:48:30 +0530

    Added

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,20 +1,13 @@
 import { FaLocationArrow } from "react-icons/fa6";
 import Image from "next/image";
 import { socialMedia } from "@/data";
-import MagicButton from "./ui/MagicButton";
+import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
       {/* background grid */}
-      <div className="w-full absolute left-0 -bottom-72 min-h-96">
-        <Image
-          src="/footer-grid.svg"
-          alt="grid"
-          fill
-          className="w-full h-full opacity-50 object-cover"
-        />
-      </div>
+      
 
       <div className="flex flex-col items-center">
         <h1 className="heading lg:max-w-[45vw]">
@@ -36,7 +29,7 @@ const Footer = () => {
 
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">
-          Copyright © 2024 Adrian Hajdin
+          Copyright © 2025 Anisha Chowdhury
         </p>
 
         <div className="flex items-center md:gap-3 gap-6">
--- components/RecentProject.tsx
+++ components/RecentProject.tsx
@@ -1,66 +1,68 @@
-"use client";
+'use client';
 
-import { FaLocationArrow } from "react-icons/fa6";
+import dynamic from 'next/dynamic';
+import { FaLocationArrow } from 'react-icons/fa6';
+import { projects } from '@/data';
 
-import { projects } from "@/data";
-import { PinContainer } from "./ui/Pin";
+// ✅ Dynamically import PinContainer with SSR disabled
+const PinContainer = dynamic(
+  () => import('./ui/3d-pin').then((mod) => mod.PinContainer),
+  {
+    ssr: false,
+    loading: () => <div>Loading...</div>, // Optional fallback while loading on client
+  }
+);
 
 const RecentProjects = () => {
   return (
     <div className="py-20">
       <h1 className="heading">
-        A small selection of{" "}
-        <span className="text-purple">recent projects</span>
+        A small selection of <span className="text-purple">recent projects</span>
       </h1>
-      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+
+      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
         {projects.map((item) => (
-          <div
-            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
-            key={item.id}
-          >
-            <PinContainer
-              title="/ui.aceternity.com"
-              href="https://twitter.com/mannupaaji"
-            >
-              <div className="relative flex items-center justify-center sm:w-96 w-[80vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10">
-                <div
-                  className="relative w-full h-full overflow-hidden lg:rounded-3xl"
-                  style={{ backgroundColor: "#13162D" }}
-                >
-                  <img src="/bg.png" alt="bgimg" />
+          <div key={item.id}
+  className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem] flex items-center justify-center sm:w-[570px] w-[80vw]"
+>
+
+            <PinContainer title={item.title} href={item.link}>
+              {/* Image Container */}
+              <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh]  mb-10'>
+                <div className="relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]">
+                  <img src="/bg.png" alt="bg-img" className="w-full h-full object-cover" />
                 </div>
                 <img
                   src={item.img}
-                  alt="cover"
-                  className="z-10 absolute bottom-0"
+                  alt={item.title}
+                  className="z-10 absolute bottom-0 object-contain"
                 />
               </div>
 
+              {/* Project Title */}
               <h1 className="font-bold lg:text-2xl md:text-xl text-base line-clamp-1">
                 {item.title}
               </h1>
 
+              {/* Project Description */}
               <p
-                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2"
-                style={{
-                  color: "#BEC1DD",
-                  margin: "1vh 0",
-                }}
+                className="lg:text-xl lg:font-normal font-light text-sm line-clamp-2 text-[#BEC1DD] my-2"
               >
                 {item.des}
               </p>
 
+              {/* Icon List and Call-to-Action */}
               <div className="flex items-center justify-between mt-7 mb-3">
                 <div className="flex items-center">
                   {item.iconLists.map((icon, index) => (
                     <div
                       key={index}
-                      className="border border-white/[.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
+                      className="border border-white/[0.2] rounded-full bg-black lg:w-10 lg:h-10 w-8 h-8 flex justify-center items-center"
                       style={{
                         transform: `translateX(-${5 * index + 2}px)`,
                       }}
                     >
-                      <img src={icon} alt="icon5" className="p-2" />
+                      <img src={icon} alt={`icon-${index}`} className="p-2" />
                     </div>
                   ))}
                 </div>
